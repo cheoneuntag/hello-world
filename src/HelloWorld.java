7309c920bf22f224b1063a700b22f3e9de60278f
Admin <Admin@192.168.1.19>
2019-01-07 11:45:57 +1300

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+   public static void main(String[] args) {
+	   System.out.println("Hello World");
+	   System.out.println("Fuck the World");
+	   System.out.println("Hello I'm your father");
+   }
+}
